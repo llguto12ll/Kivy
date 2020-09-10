bd31b30c790311660a82ba27a3bd15a0d92dec3e
guto <g.luizaugusto12@gmail.com>
2020-09-10 18:33:22 -0300

    Touch test

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
-class myApp(App):
+
+class Touch(Widget):
+    btn = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        print("Mouse Down", touch)
+        self.btn.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        print("Mouse Move", touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse UP", touch)
+        self.btn.opacity = 1
+
+
+class MyApp(App):
     def build(self):
-        return Label(text='Oi')
+        return Touch()
+
 
 if __name__ == '__main__':
-    App=myApp()
+    App = MyApp()
     App.run()
-
-
